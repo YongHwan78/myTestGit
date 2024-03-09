ba868fb65478b357078be66e623580e6257bd3ec
YongHwan78 <bluedragon3706@gmail.com>
2024-03-09 12:38:30 +0900

    new 2024.03.09
    Change-Id: I14a7b89fade1a7cac3d84c6ae844994323facb52
    Signed-off-by: YongHwan78 <bluedragon3706@gmail.com>

--- src/kr/or/ddit/basic/TestClass.java
+++ src/kr/or/ddit/basic/TestClass.java
@@ -3,6 +3,6 @@ package kr.or.ddit.basic;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hellow this is ");
-		
+		System.out.println("new project");
 	}
 }
